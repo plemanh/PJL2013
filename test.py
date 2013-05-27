99eb302599ce28f4d8866447ff075c73f7364caa
louiscoppey <louiscopey@gmail.com>
2013-05-27 12:07:14 +0200

    Test de départ
    Trace un sinus en implémentant les deux packages

---
+++ test.py
@@ -0,0 +1,10 @@
+import pylab as pl
+import numpy as np
+t = np.linspace(0, 8 * np.pi, 100)
+x = np.sin(t)
+pl.plot(t, x)
+pl.xlabel('Time')
+pl.ylabel('Amplitude')
+pl.ylim([-1.5, 1.5])
+pl.show()
+
